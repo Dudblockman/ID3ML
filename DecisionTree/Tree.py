aac7f371254151187cad27a42b64c1c7af062a99
Logan Peterson <loganpete12@gmail.com>
2021-02-22 21:57:14 -0700

    I want this to be over

--- DecisionTree/Tree.py
+++ DecisionTree/Tree.py
@@ -2,116 +2,141 @@ import numpy as np
 import re
 from collections import Counter
 
-def entropy(arr: np.array):
-    if len(arr.shape) > 1:
-        a = (arr.T / np.sum(arr, axis=1)).T
-        b = np.ma.log2(a)
-        c = np.sum(-np.multiply(a, b), axis=1)
-        d = (np.sum(arr, axis=1)/np.sum(arr))
-        e = np.sum(np.multiply(c,d))
-        return e
-    else: 
-        a = (arr.T / np.sum(arr)).T
-        b = np.ma.log2(a)
-        c = np.sum(-np.multiply(a, b))
-        d = (np.sum(arr)/np.sum(arr))
-        e = np.sum(np.multiply(c,d))
-        return e
-
-def gain(arr: np.array, arr2: np.array):
-    return entropy(arr2)-entropy(arr)
-
-def bestGainIndex(S, attributes, label):
-    coalesced = {}
-    lastIndex = attributes["count"]
-    for k,v in attributes.items():
-        if hasattr(v, "__len__"):
-            coalesced[k] = np.zeros( (len(v), len(label)), dtype=np.float ) 
-
-    for terms in S:
-        for i in range(lastIndex):
-            if i in coalesced:
-                j = attributes[i].index(terms[i])
-                k = label.index(terms[lastIndex])
-                coalesced[i][j][k] += 1
-    counts = Counter(S.T[len(S[0])-1])
-
-    arr2 = np.array([counts[x] for x in label]).T
-
-    bestIndex = -1
-    bestGain = 0
-    for k in coalesced.keys():
-        if gain(coalesced[k], arr2) > bestGain:
-            bestGain = gain(coalesced[k], arr2)
-            bestIndex = k
     
-    return bestIndex
 
-def loadfile(filename, process=True):
-    data = []
-    with open (filename, 'r') as f:
-        for line in f:
-            terms = line.strip().split(',')
-            data.append(terms)
-    return np.array(data)
+class treemaker:
+    mode = 0
+    def entropy(self, arr: np.array):
+        if len(arr.shape) > 1:
+            a = (arr.T / np.sum(arr, axis=1)).T
+            b = np.ma.log2(a)
+            c = np.sum(-np.multiply(a, b), axis=1)
+            d = (np.sum(arr, axis=1)/np.sum(arr))
+            e = np.sum(np.multiply(c,d))
+            return e
+        else: 
+            a = (arr.T / np.sum(arr)).T
+            b = np.ma.log2(a)
+            c = np.sum(-np.multiply(a, b))
+            d = (np.sum(arr)/np.sum(arr))
+            e = np.sum(np.multiply(c,d))
+            return e
 
-def getAttributes(data):
-    dat = [list(set(x)) for x in data.T]
-    [v.sort() for v in dat]
-    attributes = dat[0:len(dat)-1]
-    attributes = {i: attributes[i] for i in range(len(attributes))}
-    attributes["count"] = len(dat)-1
-    label = dat[len(dat)-1]
-    return attributes, label
+    def gain(self, arr: np.array, arr2: np.array):
+        return self.entropy(arr2)-self.entropy(arr)
 
-def preprocess(data):
-    data2 = data.copy()
-    terms, _ = getAttributes(data)
-    for key, term in terms.items():
-        if type(term) == list:
-            numeric = True
-            for element in term:
-                try:
-                    float(element)
-                except ValueError:
-                    numeric = False
-                    break
-            if numeric == True:
-                median = np.round(np.median(np.array(term).astype(np.float))).astype(np.int)
-                for i in range(len(data)):
-                    data2[i, int(key)] = ">="+str(median) if float(data[i, int(key)]) >= median else "<"+str(median)
-    return data2
+    def informationGain(self, S, attributes, label):
+        coalesced = {}
+        lastIndex = attributes["count"]
+        for k,v in attributes.items():
+            if hasattr(v, "__len__"):
+                coalesced[k] = np.zeros( (len(v), len(label)), dtype=np.float ) 
 
-def id3(S, attributes, label, maxdepth):
-    node = {}
+        for terms in S:
+            for i in range(lastIndex):
+                if i in coalesced:
+                    j = attributes[i].index(terms[i])
+                    k = label.index(terms[lastIndex])
+                    coalesced[i][j][k] += 1
+        counts = Counter(S.T[len(S[0])-1])
 
-    commonList = Counter(S.T[len(S[0])-1]).most_common()
-    mostCommon = commonList[0][0]
+        arr2 = np.array([counts[x] for x in label]).T
 
-    bestIndex = bestGainIndex(S, attributes, label)
-    if len(commonList) < 1 or bestIndex == -1:
-        return mostCommon
-    node["attribute"] = bestIndex
-    node["leaves"] = {}
-    for leaf in attributes[bestIndex]:
-        subset = np.array([x for x in S if x[bestIndex] == leaf])
-        if len(subset) == 0:
-            node["leaves"][leaf] = mostCommon
-        elif maxdepth <= 1:
-            node["leaves"][leaf] = Counter(subset.T.tolist()[len(S[0])-1]).most_common()[0][0]
+        bestIndex = -1
+        bestGain = 0
+        for k in coalesced.keys():
+            if self.gain(coalesced[k], arr2) > bestGain:
+                bestGain = self.gain(coalesced[k], arr2)
+                bestIndex = k
+        return bestIndex
+
+    def majorityError(self, S, attributes, label):
+        return 0
+
+    def gini(self, A):
+        summ = 0
+        for i, ai in enumerate(A[:-1], 1):
+            summ += np.sum(np.abs(ai - A[i:]))
+        return summ / (len(A)**2 * np.average(A))
+
+    def giniIndex(self, S, attributes, label):
+        print (str(self.gini(S)))
+        lastIndex = attributes["count"]
+        for terms in S:
+            for i in range(lastIndex):
+                if i in attributes:
+                    j = attributes[i].index(terms[i])
+                    k = label.index(terms[lastIndex])
+        return 0
+
+    def bestGainIndex(self, S, attributes, label):
+        if self.mode == 0:
+            return self.informationGain(S, attributes, label)
+        if self.mode == 1:
+            return self.majorityError(S, attributes, label)
+        if self.mode == 2:
+            return self.giniIndex(S, attributes, label)
+        return 0
+
+
+    def getAttributes(self, data):
+        dat = [list(set(x)) for x in data.T]
+        [v.sort() for v in dat]
+        attributes = dat[0:len(dat)-1]
+        attributes = {i: attributes[i] for i in range(len(attributes))}
+        attributes["count"] = len(dat)-1
+        label = dat[len(dat)-1]
+        return attributes, label
+
+    def preprocess(self, data):
+        data2 = data.copy()
+        terms, _ = self.getAttributes(data)
+        for key, term in terms.items():
+            if type(term) == list:
+                numeric = True
+                for element in term:
+                    try:
+                        float(element)
+                    except ValueError:
+                        numeric = False
+                        break
+                if numeric == True:
+                    median = np.round(np.median(np.array(term).astype(np.float))).astype(np.int)
+                    for i in range(len(data)):
+                        data2[i, int(key)] = ">="+str(median) if float(data[i, int(key)]) >= median else "<"+str(median)
+        return data2
+
+    def id3(self, S, attributes, label, maxdepth):
+        node = {}
+
+        commonList = Counter(S.T[len(S[0])-1]).most_common()
+        mostCommon = commonList[0][0]
+
+        bestIndex = self.bestGainIndex(S, attributes, label)
+        if len(commonList) < 1 or bestIndex == -1:
+            return mostCommon
+        node["attribute"] = bestIndex
+        node["leaves"] = {}
+        for leaf in attributes[bestIndex]:
+            subset = np.array([x for x in S if x[bestIndex] == leaf])
+            if len(subset) == 0:
+                node["leaves"][leaf] = mostCommon
+            elif maxdepth <= 1:
+                node["leaves"][leaf] = Counter(subset.T.tolist()[len(S[0])-1]).most_common()[0][0]
+            else:
+                newAttributes = attributes.copy()
+                del newAttributes[bestIndex]
+                node["leaves"][leaf] = self.id3(subset, newAttributes, label, maxdepth - 1)
+        return node
+
+    def generateTree(self, S, maxdepth=100, process=True, mode=0):
+        self.mode = mode
+        if process:
+            S = self.preprocess(S)
         else:
-            newAttributes = attributes.copy()
-            del newAttributes[bestIndex]
-            node["leaves"][leaf] = id3(subset, newAttributes, label, maxdepth - 1)
-    return node
-
-def generateTree(S, maxdepth=100, process=True):
-    if process:
-        S = preprocess(S)
-    else:
-        S = S.copy()
-    attributes, label = getAttributes(S)
-    return id3(S, attributes, label, maxdepth)
+            S = S.copy()
+        attributes, label = self.getAttributes(S)
+        return self.id3(S, attributes, label, maxdepth)
 
 def evalTree(T, V):
     while type(T) == dict:
@@ -140,11 +165,19 @@ def accuracy(S, T):
             correct+=1
     return correct / len(S)
 
+def loadfile(filename, process=True):
+    data = []
+    with open (filename, 'r') as f:
+        for line in f:
+            terms = line.strip().split(',')
+            data.append(terms)
+    return np.array(data)
 
 trainingdata = loadfile("DecisionTree/bank/train.csv")
 testdata = loadfile("DecisionTree/bank/test.csv")
 
-tree = generateTree(trainingdata, 16)
+treegen = treemaker()
+tree = treegen.generateTree(trainingdata, 2, mode=2)
 
 print(tree)
 print(accuracy(trainingdata, tree), accuracy(testdata, tree))
